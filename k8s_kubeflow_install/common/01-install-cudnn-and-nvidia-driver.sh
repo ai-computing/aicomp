d401f6d55d34be318222f80670fa0f566b0a29c0
etri <swsok@etri.re.kr>
2023-06-26 02:42:08 +0000

    added mlperf from https://github.com/mlcommons/training_results_v2.1/tree/main/NVIDIA/benchmarks/bert/implementations/pytorch-22.09

--- k8s_kubeflow_install/common/01-install-cudnn-and-nvidia-driver.sh
+++ k8s_kubeflow_install/common/01-install-cudnn-and-nvidia-driver.sh
@@ -15,7 +15,8 @@ whereis cuda
 #cd ~/nvidia
 CUDNN_DEB_FILE="cudnn-local-repo-ubuntu2004-8.8.0.121_1.0-1_amd64.deb"
 if ! [ -e $CUDNN_DEB_FILE ]; then
-	wget  https://developer.download.nvidia.com/compute/redist/cudnn/v8.8.0/local_installers/12.0/${CUDNN_DEB_FILE}
+	sudo apt-get -y install axel
+	axel -n 20  https://developer.download.nvidia.com/compute/redist/cudnn/v8.8.0/local_installers/12.0/${CUDNN_DEB_FILE}
 fi
 sudo dpkg -i ${CUDNN_DEB_FILE}
 sudo cp /var/cudnn-local-repo-ubuntu2004-8.8.0.121/cudnn-local-A9E17745-keyring.gpg /usr/share/keyrings/
