20492a923b5f61f22238dc71fb98718b27e291b8
swsok <swsok@etri.re.kr>
2023-09-23 00:21:56 +0900

    change gpt-neox Dockerfile and clean gpt-neox directories

--- gpt-neox/scripts_swsok/3.required_packages_install.sh
+++ gpt-neox/scripts_swsok/3.required_packages_install.sh
@@ -3,5 +3,5 @@
 sudo apt install libopenmpi-dev
 pip install mpi4py
 # urllib3 v2 conflicts with original gpt-neox codes
-pip uninstall urllib3
-pip install urllib3==1.26.16
+#pip uninstall urllib3
+#pip install urllib3==1.26.16
