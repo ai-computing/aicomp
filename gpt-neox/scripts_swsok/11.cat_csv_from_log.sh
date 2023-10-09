094e4af1e2b82af2a0257096c2918512f2f4bd1f
swsok <swsok@etri.re.kr>
2023-10-10 01:36:45 +0900

    add a script to generate csv from log file

---
+++ gpt-neox/scripts_swsok/11.cat_csv_from_log.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+	echo "Usage: $0 [log_file]"
+	exit 0
+fi
+
+if [ ! -f $1 ]; then
+	echo "$1 is missing."
+	exit 0
+fi
+
+grep lm_loss $1 | awk '{printf "%s,%s,%s,%f\n",$2,substr($8, 1, length($8)-1),substr($26,1,length($26)-6),$29}' | tee $1.csv
