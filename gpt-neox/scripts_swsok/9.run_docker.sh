9510d6744c05f836e1293be03e58e71cc67e743d
swsok <swsok@etri.re.kr>
2024-01-26 03:02:18 +0000

    reorganize configs directory

--- gpt-neox/scripts_swsok/9.run_docker.sh
+++ gpt-neox/scripts_swsok/9.run_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it --gpus=all -e NVIDIA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 --shm-size=1g --ulimit memlock=-1 --mount type=bind,src=$PWD,dst=/gpt-neox --security-opt seccomp=seccomp-docker.json swsok/gpt-neox:v7
+docker run --rm -it --network host --gpus=all -e NVIDIA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 --shm-size=1g --ulimit memlock=-1 --mount type=bind,src=$PWD,dst=/gpt-neox --mount type=bind,src=/var/nfs,dst=/data --security-opt seccomp=seccomp-docker.json swsok/gpt-neox:v8
