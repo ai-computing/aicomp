04bfadd3f06f156bd8e1c551965a94d0aac2f836
mhcha <mhcha@etri.re.kr>
2024-10-16 13:48:55 +0900

    applying a tool for FLOPS measurement

---
+++ opt_prime/examples/pp_train_gpt2-xl-flops.py
@@ -0,0 +1,163 @@
+#
+# Copyright (c) 2024-present, ETRI, All rights reserved.
+#
+
+
+import torch
+import torch.nn as nn
+
+
+import torch.distributed as dist
+import datetime
+import logging
+import os
+import sys
+import math
+import time
+
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, GPT2Config
+from datasets import load_dataset
+from torch.utils.data import DataLoader
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from opt_prime.opti_pri import Optimus_p
+
+from functools import partial
+from ptflops import get_model_complexity_info
+
+logging.basicConfig(level=logging.ERROR)
+
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2-xl")
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.pad_token_id = tokenizer.eos_token_id
+
+config = GPT2Config(use_cache=False)
+model = GPT2LMHeadModel(config)
+model = model.from_pretrained("gpt2-xl")
+
+def get_total_params(module: torch.nn.Module):
+    total_params = 0
+    for param in module.parameters():
+        total_params += param.numel()
+    return total_params
+
+
+if int(os.environ["RANK"]) == 0:
+    print ('Total parameters in model: {:,}'.format(get_total_params(model)))
+
+
+batch_size = 32
+micro_batch_size = int(os.environ["WORLD_SIZE"]) // 2 # TODO
+
+def gpt_input_constructor(input_shape, tokenizer):
+    inp_seq = ""
+    for _ in range(input_shape[1] - 2):  # [CLS], [SEP]
+        inp_seq += tokenizer.pad_token  
+
+    inputs = tokenizer([inp_seq] * input_shape[0], padding=True, truncation=True, max_length=1024,
+                       return_tensors="pt")
+    data, labels = inputs.input_ids, inputs.input_ids
+    temp = {'input_ids': data , 'labels' : labels }
+    return temp
+
+
+if int(os.environ["RANK"]) == 0:
+    print(f"total process count: {os.environ['WORLD_SIZE']}")
+    print(f"batch size: {batch_size}")
+    print(f"micro batch size: {micro_batch_size}")
+
+    # TODO
+    #macs, params = get_model_complexity_info(model, (1, 1024), as_strings=True, input_constructor=partial(gpt_input_constructor, tokenizer=tokenizer), print_per_layer_stat=False, verbose=True)
+    macs, params = get_model_complexity_info(model, (1, 1024), as_strings=True, input_constructor=partial(gpt_input_constructor, tokenizer=tokenizer), print_per_layer_stat=True, verbose=True)
+    print('>>>>>>> # of Operations:', macs, ', # of Parameters', params)
+
+optimus_p = Optimus_p(model, micro_batch_size, use_gpu=True)
+print(f" rank={optimus_p.get_rank()} ...")
+
+optimus_p.train()
+#optimizer = torch.optim.SGD(optimus_p.parameters(), lr=5.0)
+optimizer = torch.optim.Adam(optimus_p.parameters(), lr=3e-5)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
+
+datasets = load_dataset("squad").data["train"]["context"]
+datasets = [str(record) for record in datasets if len(str(record)) < 500]
+dataloader = DataLoader(datasets, batch_size=batch_size, num_workers=4)
+data_size=len(dataloader.dataset)
+print(f"data_size={data_size}")
+nbatches = len(dataloader)
+print(f"nbatches={nbatches}")
+
+
+epochs = 1 # The number of epochs
+
+def train():
+
+    optimus_p.train() # turn on the train mode
+
+    total_loss = 0
+    start_time = time.time()
+
+    for i, batch in enumerate(dataloader):
+        #if optimus_p.get_rank() == 0:
+        #    tick2 = time.time()
+
+        data, labels = None, None
+
+        # prepare input and label
+        if optimus_p.is_first_stage():
+            tokens =  tokenizer(batch, padding=True, truncation=True, max_length=1024,return_tensors="pt")
+            data, labels = tokens.input_ids, tokens.input_ids
+
+        labels = optimus_p.move_labels2last_stage(labels)
+
+        optimizer.zero_grad()
+
+        optimus_p.run(data, labels)
+
+        if optimus_p.is_last_stage():
+            loss = optimus_p.get_loss() 
+        else:
+            loss = None
+
+        torch.nn.utils.clip_grad_norm_(optimus_p.parameters(), 0.5)
+        optimizer.step()
+
+        if optimus_p.is_last_stage():
+            loss = sum(loss) / optimus_p.mbsize
+            total_loss += loss
+            log_interval = 10
+            if i % log_interval == 0 and i > 0:
+                cur_loss = total_loss / log_interval
+                elapsed = time.time() - start_time
+                print('| epoch {:3d} | {:5d}/{:5d} batches | '
+                    'lr {:02.2f} | ms/batch {:5.2f} | '
+                    'loss {:5.2f} | ppl {:8.2f}'.format(
+                        epoch, i, nbatches, scheduler.get_lr()[0],
+                        elapsed * 1000 / log_interval,
+                        cur_loss, math.exp(cur_loss)))
+
+                total_loss = 0
+                start_time = time.time()
+
+        #if optimus_p.get_rank() == 0:
+        #    tock2 = time.time()
+        #    print('Time elapsed: %.3f sec ' % (tock2-tick2))
+
+if optimus_p.get_rank() == 0:
+    tick = time.time()
+
+for epoch in range(1, epochs + 1):
+    epoch_start_time = time.time()
+    train()
+    scheduler.step()
+
+if optimus_p.get_rank() == 0:
+    tock = time.time()
+    elapsed_time = tock - tick
+
+    print('Time elapsed: %.3f sec ' % (elapsed_time))
+
+if optimus_p.get_rank() == optimus_p.get_world_size() - 1:
+    print(f"###################################")
+print(f"[rank:{optimus_p.get_rank()}, run completed ...")
