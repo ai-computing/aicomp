3b4d70aa9a7aff82118b80d74bbb2d72a93876f3
swsok <swsok@etri.re.kr>
2025-09-01 08:19:49 +0000

    add run_nccl_tests.sh to check nccl communication bandwidth

---
+++ fasop/run_nccl_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#common args
+DOCKER_IMG=swsok/nccl_tests:cuda12.9
+CONTAINER_NAME="nccl_tests_docker"
+COMMON_ARG="--gpus all --name $CONTAINER_NAME --ipc=host --network=host"
+LOG=nccl_tests_result.txt
+
+docker rm -f $CONTAINER_NAME 2>/dev/null
+
+docker run -d $COMMON_ARG $DOCKER_IMG sleep infinity
+
+EXEC_LIST=(all_gather_perf all_reduce_perf alltoall_perf broadcast_perf gather_perf hypercube_perf reduce_perf reduce_scatter_perf scatter_perf sendrecv_perf)
+
+for perf_test in "${EXEC_LIST[@]}"; do
+	echo $perf_test | tee -a $LOG
+	docker exec $CONTAINER_NAME ./build/$perf_test -b 1G -e 1G -f 2 -g 2 | grep float | awk '{print $7,$11}' | tee -a $LOG
+done
+
+docker stop $CONTAINER_NAME >/dev/null
+docker rm $CONTAINER_NAME >/dev/null
+
