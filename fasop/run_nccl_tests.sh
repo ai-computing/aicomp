446767e06f44e550f665b039f29ddb138c66c25d
swsok <swsok@etri.re.kr>
2025-09-02 05:53:25 +0000

    change output formats

--- fasop/run_nccl_tests.sh
+++ fasop/run_nccl_tests.sh
@@ -5,7 +5,7 @@ DOCKER_IMG=swsok/nccl_tests:cuda12.9
 CONTAINER_NAME="nccl_tests_docker"
 COMMON_ARG="--name $CONTAINER_NAME --ipc=host --network=host"
 LOG=nccl_tests_result.txt
-GPU_PAIR="0,1"
+GPU_PAIR="0,4"
 
 docker rm -f $CONTAINER_NAME 2>/dev/null
 
@@ -13,8 +13,10 @@ docker run -d $COMMON_ARG --gpus '"device='"$GPU_PAIR"'"' $DOCKER_IMG sleep infi
 
 EXEC_LIST=(all_gather_perf all_reduce_perf alltoall_perf broadcast_perf gather_perf hypercube_perf reduce_perf reduce_scatter_perf scatter_perf sendrecv_perf)
 
+echo "    unit=GB/s [out-of-place_bw] [bus_bw] [in-place_bw] [bus_bw]" | tee -a $LOG
+
 for perf_test in "${EXEC_LIST[@]}"; do
-	echo $perf_test | tee -a $LOG
+	echo -n "$perf_test " | tee -a $LOG
 	docker exec $CONTAINER_NAME ./build/$perf_test -b 1G -e 1G -f 2 -g 2 | grep float | awk '{print $7,$8,$11,$12}' | tee -a $LOG
 #	docker exec $CONTAINER_NAME ./build/$perf_test -b 8 -e 1G -f 2 -g 2
 done
