effcdbdf5f7a708198060f36fc2f48a3bbc88a11
mhcha <mhcha@etri.re.kr>
2025-09-05 04:22:23 +0000

    fix arg

--- opt_prime/examples/pp_train_llama4.py
+++ opt_prime/examples/pp_train_llama4.py
@@ -92,7 +92,8 @@ if int(os.environ["RANK"]) == 0:
 
 
 batch_size = 32
-micro_batch_size = int(os.environ["WORLD_SIZE"]) // 2 # TODO
+#micro_batch_size = int(os.environ["WORLD_SIZE"]) // 2 # TODO
+micro_batch_size = 4
 
 if int(os.environ["RANK"]) == 0:
     print(f"total process count: {os.environ['WORLD_SIZE']}")
@@ -101,7 +102,8 @@ if int(os.environ["RANK"]) == 0:
 
 #optimus_p = Optimus_p(model, micro_batch_size, use_gpu=True)
 #optimus_p = Optimus_p(model, micro_batch_size, use_gpu=True, activation_ckpt=True, force_free_mem=True, display_mem=True, swap_opt_in_fwdbwd=True, swap_model_in_optstep=False, ir_analyze=IR_Anal.SEQUENTIAL)
-optimus_p = Optimus_p(model, micro_batch_size, use_gpu=True, activation_ckpt=True, force_free_mem=True, display_mem=True, swap_opt_in_fwdbwd=True, swap_model_in_optstep=True, ir_analyze=IR_Anal.SEQUENTIAL)
+#optimus_p = Optimus_p(model, micro_batch_size, use_gpu=True, activation_ckpt=True, force_free_mem=True, display_mem=True, swap_opt_in_fwdbwd=True, swap_model_in_optstep=True, ir_analyze=IR_Anal.SEQUENTIAL)
+optimus_p = Optimus_p(model, micro_batch_size, use_gpu=True, activation_ckpt=False, force_free_mem=True, display_mem=True, swap_opt_in_fwdbwd=False, swap_model_in_optstep=False, ir_analyze=IR_Anal.SEQUENTIAL)
 print(f" rank={optimus_p.get_rank()} ...")
 
 optimus_p.train()
